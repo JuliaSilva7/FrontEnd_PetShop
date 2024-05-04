ab962699bdd3bfab659b782d8793de18b38b0184
Julia Silva <juliacarolina07@gmail.com>
2024-05-04 16:36:08 -0300

    Adicionando slide

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let imagens = ['assets/img/bannerpet1.jpeg', 'assets/img/bannerpet2.jpeg', 'assets/img/bannerpet3.jpeg', 'assets/img/bannerpet4.jpeg'];
+let index = 0;
+let time =5000;
+
+function slideShow() {
+    document.getElementById('imgBanner').src = imagens[index];
+    index++;
+
+    if (index == imagens.length) {
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+
+slideShow();
+
