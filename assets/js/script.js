b5b0676e219546f65c15cbf927e0b3742f3b4885
Heloisa <heloisafleury012@gmail.com>
2024-05-04 23:24:08 -0300

    Criando a seção dos produtos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let imagens = ['assets/img/bannerpet1.jpeg', 'assets/img/bannerpet2.jpeg', 'assets/img/bannerpet3.jpeg', 'assets/img/bannerpet4.jpeg'];
 let index = 0;
-let time =5000;
+let time =3000;
 
 function slideShow() {
     document.getElementById('imgBanner').src = imagens[index];
